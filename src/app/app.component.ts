09fd3c0c14d7ebb93d6cf9548de0b298b2dbed57
Fredy Whatley <fwhatley@frontlineed.com>
2018-09-07 00:51:51 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FileHub';
+  url = 'http://localhost:3000/upload';
+  filesToUpload: Array<File>;
+
+  constructor() {
+    this.filesToUpload = [];
+  }
+
+  upload() {
+    this.makeFileRequest(this.url, [], this.filesToUpload).then((result) => {
+      console.log(result);
+    }, (error) => {
+      console.error(error);
+    });
+
+  }
+
+  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <Array<File>> fileInput.target.files;
+  }
+
+  makeFileRequest(url: string, params: Array<string>, files: Array<File> ) {
+    return new Promise((resolve, reject) => {
+      const formData: any = new FormData();
+      const xhr = new XMLHttpRequest();
+      for (let i = 0; i < files.length; i++) {
+        formData.append('uploads[]', files[i], files[i].name);
+      }
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(xhr.response);
+          }
+        }
+      };
+
+      xhr.open('POST', url, true);
+      xhr.send(formData);
+
+    });
+  }
+
 }
