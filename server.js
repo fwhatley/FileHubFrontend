098da344a87621c41f781be5eb9b8dd3f87d81d8
Fredy Whatley <fwhatley@frontlineed.com>
2018-09-08 11:58:32 -0400

    add configs to serve in heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+// Install express server - https://medium.com/@shubhsharma10/how-to-deploy-angular-6-app-to-heroku-52b73ac7a3aa
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist/FileHub'));
+
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/FileHub/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+var server = app.listen(process.env.PORT || 3000, function (){
+    console.log("Listening on port %s...", server.address().port);
+})
