216dc58c6d5482e2864abef18e9f1deb323e5fa8
Fredy Whatley <fwhatley@frontlineed.com>
2018-10-07 14:22:25 -0400

    add qa tools

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  enableQaTools = true;
+  devAppUrl  = `http://localhost:3001/dashboard`;
+  prodAppUrl = `https://file-hub-frontend.herokuapp.com/dashboard`;
+  devApiUrl  = `http://localhost:5000/index.html`;
+  prodApiUrl = `notimplemented`;
+
   constructor() { }
 
   ngOnInit() {
