0edc1c109533426a8dffd41d1cd07cdc514cac43
Fredy Whatley <fwhatley@frontlineed.com>
2018-10-18 22:43:18 -0400

    add prod path to serve in prod

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/FileHub'));
+var IndexFilePath_Prod = '/var/wwww/FileHub';
+var IndexFilePath_Dev = '/dist/FileHub';
+var IndexFileFullPath_Prod = '/var/wwww/FileHub/index.html';
+var IndexFileFullPath_Dev = '/dist/FileHub/index.html';
+
+app.use(express.static(__dirname + IndexFilePath_Prod));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/FileHub/index.html'));
+    res.sendFile(path.join(__dirname + IndexFileFullPath_Prod));
 });
 
 // Start the app by listening on the default Heroku port
